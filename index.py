7e1b7b625d92fe877a15b82f087a114dcf76d100
Taiyo Hamamoto <taiyo.ocean5123ht@gmail.com>
2023-11-05 09:39:14 +0900

    パラメータとして画像を受け取り、その画像内のテキストを返すようにした。

--- index.py
+++ index.py
@@ -1,11 +1,26 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 from google.cloud import vision
-with open("test-shiba-inu.jpg", "rb") as image_file:
-    content = image_file.read()
 
-# Vision AIが扱える画像データにする
-image = vision.Image(content=content)
+app = FastAPI()
 
-# ImageAnnotatorClientのインスタンスを生成
-annotator_client = vision.ImageAnnotatorClient()
-response_data = annotator_client.label_detection(image=image)
-labels = response_data.label_annotations
+@app.get("/upload-image/")
+async def analyze_image(file: UploadFile = File(...)):
+    
+    # クライアントのインスタンスを生成
+    client = vision.ImageAnnotatorClient()
+
+    image_content = await file.read()
+
+    # Vision AIが扱える画像データにする
+    image = vision.Image(content=image_content)
+
+
+    # テキスト検出を実行
+    response = client.text_detection(image=image)
+
+    # 検出されたテキストを出力
+    texts = response.text_annotations
+    rtn_texts = [{"description": text.description} for text in texts]
+
+    return JSONResponse(content={"text": rtn_texts})
